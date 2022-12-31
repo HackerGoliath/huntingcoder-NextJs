7af116a9cb26eb8d62af24b4d441c4e7efe90aad
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2022-12-31 18:23:42 +0530

    22 Server side rendering

--- pages/blog.js
+++ pages/blog.js
@@ -4,8 +4,9 @@ import styles from '../styles/Blog.module.css'
 
 // Step 1: Collect all the files from blogadata directory
 // Step 2: Iterate through them and display them
-const Blog = () => {
-    const [blogs, setBlogs] = useState([])
+const Blog = (props) => {
+    console.log(props);
+    const [blogs, setBlogs] = useState([props.allBlogs])
     useEffect(() => {
         fetch('http://localhost:3000/api/blogs').then((a) => {
             return a.json();
@@ -22,12 +23,21 @@ const Blog = () => {
                     return <div key={blogitem.slug}>
                         <Link href={`/blogpost/${blogitem.slug}`}>
                             <h3 className={styles.blogitemh3}>{blogitem.title}</h3></Link>
-                        <p className={styles.blogitemp}>{blogitem.content.substr(0, 140)}...</p>
+                        <p className={styles.blogitemp}>{blogitem.content.substr(0, 150)}...</p>
                     </div>
                 })}
             </main>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    let data = await fetch('http://localhost:3000/api/blogs')
+    let allBlogs = await data.json()
+
+    return {
+        props: { allBlogs }, // will be passed to the page component as props
+    }
 }
 
 export default Blog
